b63737054534e06c23149bfb51c31fb558e5e488
Millie DYSON <17md0909@pupil.uvhs.org.uk>
2023-12-02 18:39:01 +0000

    trying to get webscraping to work

---
+++ NEA1/NEA1/webscraping.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+<<<<<<< HEAD
+def getHTMLdocument(url): 
+    
+    response = requests.get(url) 
+    return response.text 
+
+
+characterID = 129385
+scrapingURL = f"https://www.personality-database.com/profile/{characterID}/"
+
+html_doc = getHTMLdocument(scrapingURL) 
+
+soup = BeautifulSoup(html_doc, 'html.parser') 
+
+def printText():
+    text = soup.find('h1').get_text()
+    return text
+    # --> 'Hello!'
+
+=======
+def getHTMLdocument(url): 
+    
+    response = requests.get(url) 
+    return response.text 
+
+
+characterID = 129385
+scrapingURL = f"https://www.personality-database.com/profile/{characterID}/"
+html_doc2 = getHTMLdocument(scrapingURL) 
+
+html_doc = """
+<html>
+    <body>
+        <h1>Hello!</h1>
+    </body>
+</html>
+"""
+
+soup = BeautifulSoup(html_doc, 'html.parser') 
+
+def printText():
+    text = soup.find('h1').get_text()
+    return text
+    # --> 'Hello!'
+
+>>>>>>> 6d93915244386440ea9338132dbc3765fad5ef6e
